31182265d55027a2add19ee5e81033e8585db3c5
karybaeva13 <karybaeva.eldana13@gmail.com>
2025-10-28 21:00:20 +0600

    урок с bool командами

---
+++ fifthLesson.py
@@ -0,0 +1,24 @@
+age = input("enter your age")
+education = input("Do you have a degree?")
+
+try:
+    age = int(age)
+except:
+    print("The age is not defined")
+exit()
+
+if age > 0 and age < 6 and education == "no":
+    print("This is a preschool")
+
+elif age >= 6 and age <=18 and education == "no":
+    print("This is a school student")
+
+elif age >= 6 and age <=18 and education == "yes":
+    print("This is a college student")
+
+elif age >18 and education == "yes" or age >18 and education == "no":
+    print("This person does not study in school")
+
+else:
+    print("The information is not defined")
+
