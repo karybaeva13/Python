e4f563e5756a57fda92993c7eefb64aa9928b9e1
karybaeva13 <karybaeva.eldana13@gmail.com>
2025-10-29 20:59:42 +0600

    работа с текстовыми данными

--- fifthLesson.py
+++ fifthLesson.py
@@ -1,24 +1,23 @@
-age = input("enter your age")
-education = input("Do you have a degree?")
+age = input("enter your age: ")
+education = input("Do you have a degree? ")
 
 try:
     age = int(age)
+
+    if age > 0 and age < 6 and education == "no":
+        print("This is a preschool")
+
+    elif age >= 6 and age <=18 and education == "no":
+        print("This is a school student")
+
+    elif age >= 6 and age <=18 and education == "yes":
+        print("This is a college student")
+
+    elif age >18 and education == "yes" or age >18 and education == "no":
+        print("This person does not study in school")
+
 except:
-    print("The age is not defined")
-exit()
-
-if age > 0 and age < 6 and education == "no":
-    print("This is a preschool")
-
-elif age >= 6 and age <=18 and education == "no":
-    print("This is a school student")
-
-elif age >= 6 and age <=18 and education == "yes":
-    print("This is a college student")
-
-elif age >18 and education == "yes" or age >18 and education == "no":
-    print("This person does not study in school")
-
-else:
     print("The information is not defined")
 
+finally:
+    print("The operation is completed")
